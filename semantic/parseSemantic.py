096a6d6dbfda25928a954c662a56f1513bf6b606
JuanLoncharich <juanandresloncha@gmail.com>
2024-04-24 11:45:01 -0300

    Solucion bug de longitud de palabras menor a terminacion mas larga

--- semantic/parseSemantic.py
+++ semantic/parseSemantic.py
@@ -5,31 +5,31 @@ wordEndings = {
     "doras": 5,
     "ría": 3,
 }
-maxLen = 0
-
-for key in wordEndings:
-    if len(key) > maxLen:
-        maxLen = len(key)
+'''
+Lo que hice fue sacar la validacion de que la longitud sea mas larga que la terminación mas larga, asi
+simplemente usando la longitud que tenga la palabra, busca la raiz de la palabra
+'''
 
 def parseSemantic(word):
     length = len(word) #O(1)
     endOfWord = ''
-    if length < maxLen:
-        return word
-    for i in range(maxLen): 
+    for i in range(len(word)):
         endOfWord = word[length-i-1] + endOfWord
         # buscar en diccionario
         if endOfWord in wordEndings: #O(1)
             # cortar la palabra
             word = word[:length - wordEndings[endOfWord]]
             newLength = len(word)
+            ## si la palabra es vacía retorna ''
+            if newLength == 0:
+                return word
             if word[newLength-1] == 'a' or word[newLength-1] == 'o':
                 word = word[:newLength-1] + '@'
             return word
     return word
- 
 
+print(parseSemantic('adoras'))
 print(parseSemantic('fumador'))
-print(parseSemantic('fumadores'))
-print(parseSemantic('fumador'))
+print(parseSemantic('fumaría'))
+print(parseSemantic('fumar'))
 
